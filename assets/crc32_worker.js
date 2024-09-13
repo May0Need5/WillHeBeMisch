bfd852a21d05c4b0003a72b3ea1544485c67037b
D0ng3r <128439825+May0Need5@users.noreply.github.com>
2024-09-13 00:24:14 -0400

    Add files via upload

---
+++ assets/crc32_worker.js
@@ -0,0 +1,2 @@
+const CRC32_TABLE=(function(){var c,crcTable=[];for(var n=0;n<256;n++){c=n;for(var k=0;k<8;k++)c=((c&1)?(0xedb88320^(c>>>1)):(c>>>1));crcTable[n]=c;}return crcTable;}());function crc32(u8array,headerSize,ignoreLast4Bytes){var data=headerSize?new Uint8Array(u8array.buffer,headerSize):u8array;var crc=0^(-1);var len=ignoreLast4Bytes?data.length-4:data.length;for(var i=0;i<len;i++)crc=(crc>>>8)^CRC32_TABLE[(crc^data[i])&0xff];return((crc^(-1))>>>0);}
+onmessage=function(e){let res=crc32(e.data.data);postMessage({'crc32':res,'data':e.data.data},[e.data.data.buffer]);}
